fdec7e04e2454687cad9e784dfba599d16ef6e85
Mohamed Amin Hadj Kacem <medaminhk27@gmail.com>
2025-02-17 15:47:57 +0100

    add header in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import { QuizProvider } from './contexts/QuizContext.jsx';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QuizProvider>
+      <h1>Maynou</h1>
       <App />
     </QuizProvider>
   </React.StrictMode>
